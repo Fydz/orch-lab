2b979b0cc1fdba5af8ba035fa5b912d613fed158
Alex Chauvin <ach@meta-x.org>
2017-11-20 17:08:58 +0100

    add keys

---
+++ lab2-ansible/aws.tf
@@ -0,0 +1,58 @@
+resource "aws_security_group" "default" {
+  name = "terraform-test"
+
+  # Allow SSH & HTTP in
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH in"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP in"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Enable ICMP
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "test-srv-01" {
+  ami                    = "ami-acd005d5"
+  instance_type          = "t2.nano"
+  key_name               = "${aws_key_pair.default.id}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+
+  tags {
+    Name = "test01"
+  }
+}
+
+# ------------------------
+
+#resource "aws_eip" "test01" {
+#  instance = "${aws_instance.test-srv-01.id}"
+#  vpc      = true
+#}
+
+output "ip" {
+    # value = "${aws_eip.test01.public_ip}"
+    value = "${aws_instance.test-srv-01.public_ip}"
+}
